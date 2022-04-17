86a5c94971eaa51724db9f7540eaa5f8823211f1
CarrieMCorcoran <99152281+CarrieMCorcoran@users.noreply.github.com>
2022-04-16 22:58:21 -0400

    Add files via upload

---
+++ src/main/java/com/example/LuckyStarJar/LuckyStarJarRestApplication.java
@@ -0,0 +1,9 @@
+package com.example.LuckyStarJar;
+
+
+import jakarta.ws.rs.core.Application;
+import jakarta.ws.rs.ApplicationPath;
+
+@ApplicationPath("system")
+public class LuckyStarJarRestApplication extends Application {
+}
---
+++ src/main/java/com/example/LuckyStarJar/Person.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.LuckyStarJar;
+
+/**
+ *
+ * @author Carrie
+ */
+public class Person {
+    
+    public String name;
+    public String email;
+    
+    public Person()
+    {
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+}
