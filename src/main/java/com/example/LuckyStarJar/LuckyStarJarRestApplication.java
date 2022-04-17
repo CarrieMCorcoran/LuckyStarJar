8386ac2e4153ccc92d7ea2c819f2d62c966cf23e
CarrieMCorcoran <99152281+CarrieMCorcoran@users.noreply.github.com>
2022-04-16 22:57:53 -0400

    Delete src/main directory

--- src/main/java/com/example/LuckyStarJar/LuckyStarJarRestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.example.LuckyStarJar;
-
-
-import jakarta.ws.rs.core.Application;
-import jakarta.ws.rs.ApplicationPath;
-
-@ApplicationPath("system")
-public class LuckyStarJarRestApplication extends Application {
-}
